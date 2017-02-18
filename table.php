10b9ca4518d47fcaf44831828268d31273ff339c
bprayush <bprayush@yahoo.com>
2017-02-18 18:22:36 +0545

    Add files via upload

--- table.php
+++ table.php
@@ -1,6 +1,14 @@
 <?php
 	
 	//write the code for connecting to database here
+		$db_host = "localhost";
+		$db_user = "root";
+		$db_key = "";
+		$db_name = "mydatabase";
+
+		mysql_connect($db_host, $db_user, $db_key) or die(mysql_error());
+		$connection = mysql_select_db($db_name) or die(mysql_error());
+	
 
 ?>
 
@@ -17,13 +25,22 @@
 	<tr>
 		<th>User</th>
 		<th>Age</th>
+		<th>Gender</th>
 	</tr>
-	<?php //remember we used loop toprint all the data in the table? ?>
+	<?php //remember we used loop toprint all the data in the table? 
+	$query = "SELECT * FROM table_name";
+		$result = mysql_query($query) or die(mysql_error());
+	
+
+		while($data=mysql_fetch_assoc($result))
+		{
+	?>
 		<tr>
-			<td><?php   //echo the name here fetched from database?></td>
-			<td><?php    //echo the age here fetched from database?></td>
+			<td><?php  echo $data['Name'];?></td>
+			<td><?php    echo $data['Age'];//echo the age here fetched from database?></td>
+			<td><?php    echo 	$data['Gender'];//echo the gender here fetched from database?></td>
 		</tr>
-	<?php ?>
+	<?php }?>
 
 </table>
 
